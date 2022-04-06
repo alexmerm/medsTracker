5fc1dabd4062ccfef081dc95738fa5c348aa0e73
alexmerm <1852285+alexmerm@users.noreply.github.com>
2022-04-06 17:56:41 -0400

    Dropping Coredata things to make simpler view

--- medzTracker/ContentView.swift
+++ medzTracker/ContentView.swift
@@ -13,14 +13,6 @@ import CoreData
 
 
 struct ContentView: View {
-//    CoreData things
-//    @Environment(\.managedObjectContext) private var viewContext
-//
-//    @FetchRequest(
-//        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-//        animation: .default)
-//    private var items: FetchedResults<Item>
-    
     
     @ObservedObject var viewModel : MedicineTracker //ViewModel will be passed in
 
@@ -67,37 +59,6 @@ struct ContentView: View {
             //TODO: Navigation Bar
         }
     }
-//    // MARK: CoreData Template Funcs
-//    private func addItem() {
-//        withAnimation {
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
-//
-//    private func deleteItems(offsets: IndexSet) {
-//        withAnimation {
-//            offsets.map { items[$0] }.forEach(viewContext.delete)
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
 }
 
 private let itemFormatter: DateFormatter = {
